0d8937d432ec0e8cd0cb4bd36f7c648b8971d258
David O'Connell <david.oconnell@iredave.com>
2022-07-06 10:34:46 -0700

    fixed

--- src/js/contactForm.js
+++ src/js/contactForm.js
@@ -8,12 +8,13 @@ import axios from "axios";
 const botpoison = new Botpoison({
   publicKey: "pk_be97bab5-36e3-4a62-8304-ce65c4085853",
 });
-const FORMSPARK_ACTION_URL = "https://submit-form.com/wGiMqFLH";
-
-
-
-
-
+const FORMSPARK_ACTION_URL = "https://submit-form.com/EV9uRLpK";
+const CONFIG = {
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+};
 
 let website = "";
 window.contactForm = () => {
@@ -25,28 +26,36 @@ window.contactForm = () => {
     buttonText: "Let's Talk",
     loading: false,
     async submit() {
-     
       this.data.website = website;
       this.buttonText = "Submitting...";
       this.loading = true;
 
       const { solution } = await botpoison.challenge();
-      await axios.post(FORMSPARK_ACTION_URL, {
-        message: JSON.stringify(this.data),
-
-        _botpoison: solution,
-      })
-      .then(function (response) {
-        bFormAnimation();
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-      .finally(() => {
+ 
+      await axios
+        .post(
+          FORMSPARK_ACTION_URL,
+          {
+            name: this.data.name,
+            email: this.data.email,
+            phone: this.data.phone,
+            website: this.data.website,
+            message: this.data.message,
+            _botpoison: solution,
+          },
+          CONFIG
+        )
+        .then(function (response) {
+          bFormAnimation();
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .finally(() => {
           this.data.message = "";
           this.buttonText = "Submit";
           this.loading = false;
-      });
+        });
     },
   };
 };
